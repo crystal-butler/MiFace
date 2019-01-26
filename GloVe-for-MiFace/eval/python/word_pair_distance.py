469c4090af041e979c56a0622075381ce290ecc4
Crystal Butler <crystal.butler@nyu.edu>
2019-01-25 20:03:11 -0500

    fixed bug in os.walk block for file-based calculations

--- GloVe-for-MiFace/eval/python/word_pair_distance.py
+++ GloVe-for-MiFace/eval/python/word_pair_distance.py
@@ -6,10 +6,11 @@ np.seterr(divide='ignore', invalid='ignore')  # fix divide runtime error (when d
 
 parser = argparse.ArgumentParser()
 parser.add_argument('vocab_file', help='a file of vocabulary words', type=str)
-parser.add_argument('vectors_file', type=str)
-parser.add_argument('--source_dir', help="a directory containing lists of word pairs; requires output_dir to work",
+parser.add_argument('vectors_file', help='a file of word-feature vectors based on the vocabulary', type=str)
+parser.add_argument('--source_dir', help="a directory containing lists of word pairs; requires a value for output_dir \
+to be input to work",
                     default=None, type=str)
-parser.add_argument('--output_dir', help="a directory to write similarity values to", default=None, type=str)
+parser.add_argument('--output_dir', help="a directory to write similarity value files to", default=None, type=str)
 args = parser.parse_args()
 
 
@@ -19,10 +20,9 @@ def generate():
     with open(args.vectors_file, 'r') as f:
         vectors = {}
         for line in f:
-            # Populate the dictionary with word feature lists, indexed by word
+            # Populate the dictionary with word feature lists, indexed by word.
             vals = line.rstrip().split(' ')
             vectors[vals[0]] = [float(x) for x in vals[1:]]
-        print
 
     # Create word:index and index:word maps.
     vocab = {w: idx for idx, w in enumerate(words)}
@@ -31,7 +31,6 @@ def generate():
     # Create a word x feature matrix.
     vocab_size = len(words)
     vector_dim = len(vectors[ivocab[0]])
-    print("vocab_size is %d and vector_dim is %d" % (vocab_size, vector_dim))
     W = np.zeros((vocab_size, vector_dim))
     for word, v in vectors.items():
         if word == '<unk>':
@@ -62,7 +61,8 @@ def distance(W, vocab, ivocab, input_term1, input_term2):
     index1 = vocab[input_term1]
     index2 = vocab[input_term2]
 
-    # Set the first word's distance to itself to one, for a perfect match.
+    # Set the first word's distance to itself to one for a perfect match, then
+    # look up the distance to the second word.
     dist[index1] = 1
     return dist[index2]
 
@@ -72,25 +72,25 @@ if __name__ == "__main__":
     if args.source_dir is not None and args.output_dir is not None:
         # We are reading from one or more files containing word pair lists, and writing
         # pairwise similarity scores to an output file.
-        with open(args.source_dir, 'r') as root_dir:
-            for subdir, dirs, files in os.walk(root_dir):
-                for file in files:
-                    f = open(subdir + '/' + file, 'r')
-                    f2 = open(args.output_dir + file, 'w')
-                    for line in f:
-                        array = []
-                        for word in line.split():
-                            array.append(word)
-                        input_term1 = array[0]
-                        input_term2 = array[1]
-                        similarity = distance(W, vocab, ivocab, input_term1, input_term2)
-                        if similarity == -1:
-                            # One of the words wasn't in the vocabulary, so write nothing to the file.
-                            continue
-                        else:
-                            f2.write("%s%s%s\n" % (input_term1.ljust(20), input_term2.ljust(20), similarity))
-                    f2.close()
-                    f.close()
+        # with open(args.source_dir, 'r') as root_dir:
+        for subdir, dirs, files in os.walk(args.source_dir):
+            for file in files:
+                f = open(subdir + '/' + file, 'r')
+                f2 = open(args.output_dir + file, 'w')
+                for line in f:
+                    array = []
+                    for word in line.split():
+                        array.append(word)
+                    input_term1 = array[0]
+                    input_term2 = array[1]
+                    similarity = distance(W, vocab, ivocab, input_term1, input_term2)
+                    if similarity == -1:
+                        # One of the words wasn't in the vocabulary, so write nothing to the file.
+                        continue
+                    else:
+                        f2.write("%s%s%s\n" % (input_term1.ljust(20), input_term2.ljust(20), similarity))
+                f2.close()
+                f.close()
     else:
         # Get pairs of words from the command line and print similarity scores to standard out.
         while True:
@@ -105,4 +105,4 @@ if __name__ == "__main__":
                 if similarity == -1:
                     print("Oops! One of your words wasn't in the vocabulary.")
                 else:
-                    print("From 0 to 1, the simlarity of %s and %s is %f." % (input_term1, input_term2, similarity))
+                    print("From -1 to 1, the simlarity of %s and %s is %f." % (input_term1, input_term2, similarity))
