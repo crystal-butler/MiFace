625b193326c85a31c0dbd09bf727c99cd9d28399
Crystal Butler <crystal.butler@nyu.edu>
2019-04-17 15:40:09 -0400

    added stopwords; discard words < 3 letters

--- Python/tokenize_corpus.py
+++ Python/tokenize_corpus.py
@@ -17,12 +17,16 @@ parser.add_argument('corpus_file', help='text file, to be broken into individual
 parser.add_argument('token_file', help='filename to which tokens will be written', type=str)
 args = parser.parse_args()
 
-# Don't include common words or part-of-speech designations in training files.
-stoplist = ["adj", "adjective", "adv", "all", "an", "and", "are", "as", "at", "be", "been", "but", "by", "can",
-            "for", "had", "has", "have", "he", "her", "hers", "his", "if", "in", "is", "it",
-            "nor", "noun", "of", "on", "or", "our", "ours", "phrasal", "she", "so", "some",
-            "than", "that", "the", "them", "they", "their", "theirs", "this", "to", "us",
-            "verb", "was", "we", "what", "who", "with", "you"]
+# Don't include common words, part-of-speech designations or
+# other dictionary specifiers in training files.
+# Remove HTML tags.
+stoplist = ["abbreviate", "adj", "adjective", "adv", "adverb", "all", "an", "and", "are", "as", "at",
+            "be", "been", "blockquote", "but", "by", "can", "col", "def", "defn", "etc", "ets", "ety", "etym",
+            "fld", "for", "from", "had", "has", "have", "he", "her", "hers", "his",
+            "if", "in", "into", "is", "it", "mark", "mcol", "nor", "note", "noun",
+            "obs", "of", "on", "only", "or", "our", "ours", "page", "phrasal", "she", "so", "some", "suffix", "syn",
+            "take", "than", "that", "the", "them", "they", "their", "theirs", "this", "to", "us",
+            "verb", "was", "we", "what", "who", "with", "xpage", "you"]
 
 
 # Read in the corpus file, and split tokens to the output file.
@@ -34,9 +38,9 @@ def create_token_list(corpus_file, token_file):
             temp = (re.findall(r'[a-zA-Z]+[\_\-\']?[a-zA-Z]+', line))
             cleaned = []
             for t in temp:
-                # Don't write out single letters or stoplist words.l
+                # Don't write out stoplist words or two-letter words.
                 # Transform all to lowercase for standardization.
-                if (t.lower() not in stoplist):
+                if ((len(t.lower()) > 2) and (t.lower() not in stoplist)):
                     cleaned.append(t.lower())
             for c in cleaned:
                 outfile.write("{} ".format(c))
