4005263dca3f0d5dcc4c5579312e2d4408279b43
Crystal Butler <crystal.butler@arup.com>
2020-06-06 18:21:05 -0400

    add tokenization to finetuning module

--- Python/finetune_GloVe.py
+++ Python/finetune_GloVe.py
@@ -3,6 +3,8 @@
 import mittens
 import csv
 import numpy as np
+from spacy.lang.en import English
+
 
 # Convert GloVe embeddings to a dictionary for use with Mittens.
 def embeddings_to_dict(embeddings_path):
@@ -12,7 +14,23 @@ def embeddings_to_dict(embeddings_path):
                 for line in reader}
     return embed
 
+
+def tokenize_text(text_path):
+    nlp = English()
+    # Create a Tokenizer with the default settings for English
+    # including punctuation rules and exceptions
+    tokenizer = nlp.Defaults.create_tokenizer(nlp)
+    tokens = []
+    with open(text_path, encoding='utf-8') as f:
+        for line in f:
+            tokens.append(tokenizer(line))
+    
+
+
 if __name__=='__main__':
     embeddings_path = "data/glove.840B.300d.txt"
+    text_path = "data/all_dictionaries_synonyms_cleaned_sorted.txt"
     converted_embeddings = embeddings_to_dict(embeddings_path)
     print(f'Length of the embeddings dictionary is {len(converted_embeddings)}.')
+    tokens = tokenize_text(text_path)
+    print(f'Length of the tokens array is {len(tokens)}.')
