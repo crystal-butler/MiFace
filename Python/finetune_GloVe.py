a5fa8af864df27e432fb5025693cda436d53c76a
Crystal Butler <crystal.butler@arup.com>
2020-06-10 08:53:22 -0400

    edit commands to test MIttens fine-tuning

--- Python/finetune_GloVe.py
+++ Python/finetune_GloVe.py
@@ -38,17 +38,17 @@ def execute_C(command):
 
 if __name__=='__main__':
     embeddings_path = "data/glove.840B.300d.txt"
-    text_path = "data/all_dictionaries_synonyms_cleaned_sorted.txt"
-    tokenized_text_path = "data/all_dicts_syns_tokenized.txt"
-    create_vocab_command = ["./GloVe/build/vocab_count < data/all_dicts_syns_tokenized.txt > data/all_dicts_syns_vocab.txt"]
-    create_cooccurrence_matrix_command = ["./GloVe/build/cooccur -vocab-file data/all_dicts_syns_vocab.txt < data/all_dicts_syns_tokenized.txt > data/all_dicts_syns_cooccurrences.bin"]
-    create_cooccurrence_shuffle_command = ["./GloVe/build/shuffle -memory = 8.0 < data/all_dicts_syns_cooccurrences.bin > data/all_dicts_syns_cooccurrences.shuf.bin"]
-    create_model_command = ["./GloVe/build/glove -input-file data/all_dicts_syns_cooccurrences.bin -vocab-file data/all_dicts_syns_vocab.txt -vector-size 300 -iter 100 -eta .01 -save-file data/embeddings_word/embeddings_all_dicts_syns-300-100"]
-    # converted_embeddings = embeddings_to_dict(embeddings_path)
-    # print(f'Length of the embeddings dictionary is {len(converted_embeddings)}.')
-    # tokenize_text(text_path, tokenized_text_path)
-    # print(f'Tokenized text saved to {tokenized_text_path}.')
-    # execute_C(create_vocab_command)
-    # execute_C(create_cooccurrence_matrix_command)
-    # execute_C(create_cooccurrence_shuffle_command)
-    execute_C(create_model_command)
+    text_path = "data/all_dicts_syns_filtered.txt"
+    tokenized_text_path = "data/all_dicts_syns_filtered_tokenized.txt"
+    create_vocab_command = ["./GloVe/build/vocab_count < data/all_dicts_syns_filtered_tokenized.txt > data/all_dicts_syns_filtered_vocab.txt"]
+    create_cooccurrence_matrix_command = ["./GloVe/build/cooccur -vocab-file data/all_dicts_syns_filtered_vocab.txt < data/all_dicts_syns_filtered_tokenized.txt > data/all_dicts_syns_filtered_cooccurrences.bin"]
+    create_cooccurrence_shuffle_command = ["./GloVe/build/shuffle -memory = 8.0 < data/all_dicts_syns_filtered_cooccurrences.bin > data/all_dicts_syns_filtered_cooccurrences.shuf.bin"]
+    create_model_command = ["./GloVe/build/glove -input-file data/all_dicts_syns_filtered_cooccurrences.bin -vocab-file data/all_dicts_syns_filtered_vocab.txt -vector-size 300 -iter 100 -eta .01 -save-file data/embeddings_word/embeddings_all_dicts_syns_filtered-300-100"]
+    converted_embeddings = embeddings_to_dict(embeddings_path)
+    print(f'Length of the embeddings dictionary is {len(converted_embeddings)}.')
+    tokenize_text(text_path, tokenized_text_path)
+    print(f'Tokenized text saved to {tokenized_text_path}.')
+    execute_C(create_vocab_command)
+    execute_C(create_cooccurrence_matrix_command)
+    execute_C(create_cooccurrence_shuffle_command)
+    # execute_C(create_model_command)
