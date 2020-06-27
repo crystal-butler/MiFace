6f2dc9c483c4249734e500f23bcd41c5c7288da9
Crystal Butler <crystal.butler@arup.com>
2020-06-27 13:33:02 -0400

    loop over scores and labels lists in a directory

--- Python/cluster_synonymy_scores.py
+++ Python/cluster_synonymy_scores.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 import pandas as pd
 import numpy as np
@@ -14,8 +15,8 @@ args = parser.parse_args()
 # Read in lists of scores and labels, converting scores to a distance array and labels to a label array.
 def make_arrays(scores_path, labels_path):
     """Read scores and labels in from files. Convert them to ndarrays for clustering."""
-    pairs_scores = pd.read_csv(score_path, header=None)
-    labels = pd.read_csv(label_path, header=None)
+    pairs_scores = pd.read_csv(scores_path, header=None)
+    labels = pd.read_csv(labels_path, header=None)
     # Transform similarity (proximity) scores to distances.
     pairs_distances = 1 - pairs_scores
     distances_array = np.array(pairs_distances[0][:])
@@ -24,7 +25,7 @@ def make_arrays(scores_path, labels_path):
 
 
 if __name__=='__main__':
-    if args.scores_dir is not None and args.labels_dir is not None and args.clustering_dir is not None:
+    if (os.path.isdir(args.scores_dir) and os.path.isdir(args.labels_dir) and os.path.isdir(args.clustering_dir)):
         # We are reading from one or more files containing word pair synonymy scores
         # and their associated labels, clustering the distances between scores,
         # generating a dendrogram and some statistics from the clustering, and writing
@@ -35,9 +36,24 @@ if __name__=='__main__':
             if os.path.isfile(os.path.join(args.scores_dir, entry)):
                 print(entry)
                 score_files.append(entry)
-        for entry in os.listdir(args.labels_path):
+        for entry in os.listdir(args.labels_dir):
             if os.path.isfile(os.path.join(args.labels_dir, entry)):
                 print(entry)
                 label_files.append(entry)
+        if (len(score_files) < 1 or len(label_files) < 1):
+            print ("Either scores or labels file list is empty: quitting!")
+            sys.exit()
+        if (len(score_files) != len(label_files)):
+            print("The number of scores files doesn't match the number of labels files: quitting!")
+            sys.exit()
+
+        for i in range(len(score_files)):
+            distances_array, labels_array = make_arrays(os.path.join(args.scores_dir, score_files[i]), \
+                os.path.join(args.labels_dir, label_files[i]))
+            expected_distances_count = int((len(labels_array) * (len(labels_array) - 1)) / 2)
+            if (expected_distances_count != len(distances_array)):
+                print(f'The number of values in the distances list is {len(distances_array)}, but it should be {expected_distances_count}.')
+                print('Skipping...')
+                break
     else:
         print("Be sure to include options for scores, labels and output directories when calling this module.")
