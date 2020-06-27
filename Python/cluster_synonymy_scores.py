b36ccdc686a87c4418d339f1148416fe4845059b
Crystal Butler <crystal.butler@arup.com>
2020-06-27 12:54:20 -0400

    test reading from input directories

--- Python/cluster_synonymy_scores.py
+++ Python/cluster_synonymy_scores.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('scores_dir', help='full path to a directory containing all pairs synonymy scores', type=str)
 parser.add_argument('labels_dir', help='full path to a directory containing labels for all pairs synonymy scores', type=str)
 parser.add_argument('clustering_dir', help='full path to a directory where clustering output will be written', type=str)
+args = parser.parse_args()
 
 # Read in lists of scores and labels, converting scores to a distance array and labels to a label array.
 def make_arrays(scores_path, labels_path):
@@ -23,9 +24,20 @@ def make_arrays(scores_path, labels_path):
 
 
 if __name__=='__main__':
-    if args.scores_dir is not None and args.labels_dir is not None:
-        # We are reading from one or more files containing word pair lists, and writing
-        # pairwise relatedness scores to an output file.
-        if os.path.isdir(args.source_dir):
-            for subdir, dirs, files in os.walk(args.source_dir):
-                for file in files:
+    if args.scores_dir is not None and args.labels_dir is not None and args.clustering_dir is not None:
+        # We are reading from one or more files containing word pair synonymy scores
+        # and their associated labels, clustering the distances between scores,
+        # generating a dendrogram and some statistics from the clustering, and writing
+        # that output to a file.
+        score_files = []
+        label_files = []
+        for entry in os.listdir(args.scores_dir):
+            if os.path.isfile(os.path.join(args.scores_dir, entry)):
+                print(entry)
+                score_files.append(entry)
+        for entry in os.listdir(args.labels_path):
+            if os.path.isfile(os.path.join(args.labels_dir, entry)):
+                print(entry)
+                label_files.append(entry)
+    else:
+        print("Be sure to include options for scores, labels and output directories when calling this module.")
