4b039993322f117bac1febdd98dc975ee6a8196b
Crystal Butler <crystal.butler@arup.com>
2020-06-27 20:45:05 -0400

    sort directory contents before creating input lists

--- Python/cluster_synonymy_scores.py
+++ Python/cluster_synonymy_scores.py
@@ -13,21 +13,46 @@ parser.add_argument('clustering_dir', help='full path to a directory where clust
 parser.add_argument('dendro_cutoff', help='the cutoff value for agglomerative hierarchical clustering', type=float)
 args = parser.parse_args()
 
+def make_input_lists():
+    scores_files = []
+    labels_files = []
+    for entry in sorted(os.listdir(args.scores_dir)):
+        if os.path.isfile(os.path.join(args.scores_dir, entry)):
+            scores_files.append(entry)
+    for entry in sorted(os.listdir(args.labels_dir)):
+        if os.path.isfile(os.path.join(args.labels_dir, entry)):
+            labels_files.append(entry)
+    if (len(scores_files) < 1 or len(labels_files) < 1):
+        print ("Either scores or labels file list is empty: quitting!")
+        sys.exit()
+    if (len(scores_files) != len(labels_files)):
+        print("The number of scores files doesn't match the number of labels files: quitting!")
+        sys.exit()
+    return scores_files, labels_files
 
-# Read in lists of scores and labels, converting scores to a distance array and labels to a label array.
+
 def make_arrays(scores_path, labels_path):
-    """Read scores and labels in from files. Convert them to ndarrays for clustering."""
+    """Read scores and labels in from files. Convert them to ndarrays for clustering.
+    Transform similarity (proximity) scores to distances."""
     pairs_scores = pd.read_csv(scores_path, header=None)
     labels = pd.read_csv(labels_path, header=None)
-    # Transform similarity (proximity) scores to distances.
     pairs_distances = 1 - pairs_scores
     distances_array = np.array(pairs_distances[0][:])
     labels_array = np.array(labels[0][:])
+    assert(len(pairs_scores[0] == len(distances_array)))
+    assert(len(labels[0]) == len(labels_array))
     return distances_array, labels_array
 
 
+def check_expected_distances_count(labels_array):
+    """The distances array should be a serialized upper triangular label x label matrix,
+       with entries below the diagonal omitted."""
+    expected_distances_count = int((len(labels_array) * (len(labels_array) - 1)) / 2)
+    return expected_distances_count
+
+
 def build_linkage_matrix(distances_array):
-    # Create the linkage matrix Z (perform hierarchical/agglomerative clustering).
+    """Create the linkage matrix Z (perform hierarchical/agglomerative clustering)."""
     linkage_matrix = sch.linkage(distances_array, 'average')
     # Fix distances that have become less than 0 due to floating point errors.
     for i in range(len(linkage_matrix)):
@@ -97,34 +122,21 @@ def classify_pass_fail(pct):
 
 if __name__=='__main__':
     if (os.path.isdir(args.scores_dir) and os.path.isdir(args.labels_dir) and os.path.isdir(args.clustering_dir)):
-        # We are reading from one or more files containing word pair synonymy scores
-        # and their associated labels, clustering the distances between scores,
-        # generating a dendrogram and some statistics from the clustering, and writing
-        # that output to a file.
-        score_files = []
-        label_files = []
-        for entry in os.listdir(args.scores_dir):
-            if os.path.isfile(os.path.join(args.scores_dir, entry)):
-                score_files.append(entry)
-        for entry in os.listdir(args.labels_dir):
-            if os.path.isfile(os.path.join(args.labels_dir, entry)):
-                label_files.append(entry)
-        if (len(score_files) < 1 or len(label_files) < 1):
-            print ("Either scores or labels file list is empty: quitting!")
-            sys.exit()
-        if (len(score_files) != len(label_files)):
-            print("The number of scores files doesn't match the number of labels files: quitting!")
-            sys.exit()
-
-        for i in range(len(score_files)):
-            distances_array, labels_array = make_arrays(os.path.join(args.scores_dir, score_files[i]), \
-                os.path.join(args.labels_dir, label_files[i]))
-            expected_distances_count = int((len(labels_array) * (len(labels_array) - 1)) / 2)
-            # The distances array should be a serialized upper triangular label x label matrix,
-            # with entries below the diagonal omitted.
+        """We are reading from one or more files containing word pair synonymy scores
+        and their associated labels, clustering the distances between scores,
+        generating a dendrogram and some statistics from the clustering, and writing
+        that output to a file."""
+        scores_files, labels_files = make_input_lists()
+        for i in range(len(scores_files)):
+            scores_file = os.path.join(args.scores_dir, scores_files[i])
+            labels_file = os.path.join(args.labels_dir, labels_files[i])
+            print(f'Creating arrays from {scores_file} and {labels_file}...')
+            distances_array, labels_array = make_arrays(scores_file, labels_file)
+            expected_distances_count = check_expected_distances_count(labels_array)
             if (expected_distances_count != len(distances_array)):
-                print(f'The number of values in the {score_files[i]} distances list is {len(distances_array)}, but it should be {expected_distances_count}.')
+                print(f'The number of values in the {scores_file} distances list is {len(distances_array)}, but it should be {expected_distances_count}.')
                 print('Skipping...')
+                input("Press Enter to continue...")
                 continue
             
             linkage_matrix = build_linkage_matrix(distances_array)
@@ -133,11 +145,11 @@ if __name__=='__main__':
             stats_printout = format_cluster_stats(cophenetic_coefficient, cluster_membership, pct)
 
             # Title the dendrogram, using the labels file name.
-            dendro_name = extract_dendro_name(label_files[i])
+            dendro_name = extract_dendro_name(labels_files[i])
             # Set up the plot.
             plt.figure(figsize=(14, 8.5))  # (width, height) in inches
-            # title = "Image: " + dendro_name
-            # plt.title(title, fontsize=18)
+            title = "Image: " + dendro_name
+            plt.title(title, fontsize=18)
             plt.rc('ytick',labelsize=16)
             y_label = 'Cophenetic Coefficient (Cutoff: ' + str(args.dendro_cutoff) + ')'
             plt.ylabel(y_label, fontsize=16)
@@ -159,7 +171,7 @@ if __name__=='__main__':
             with open(stats_file, 'w') as f_stat:
                 f_stat.write(stats_printout)
             plt.savefig(dendro_file, format='png')
-            # plt.show()
+            plt.show()
 
     else:
         print("Be sure to include options for scores, labels and output directories when calling this module.")
