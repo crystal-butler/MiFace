76d4b48d0839f48c4a4fd565d5dad7f40f22e59e
Crystal Butler <crystal.butler@arup.com>
2020-06-28 17:29:30 -0400

    normalize distribution scores

--- Python/visualize_distribution.py
+++ Python/visualize_distribution.py
@@ -18,6 +18,8 @@ def make_output_subdir():
 
 
 def concatenate_scores():
+    """Get all the scores from a set of files in a directory,
+    and put them into a single list."""
     score_count = 0
     all_scores = []
     for entry in sorted(os.listdir(args.scores_dir)):
@@ -38,17 +40,26 @@ def sort_scores(scores):
 
 
 def make_array(scores):
+    """Turn a list into a numpy array."""
     scores_array = np.array(scores).astype(np.float)
-    scores_array = scores_array.round(decimals=6)
     return(scores_array)
 
 
 def trim_scores(scores_array):
+    """Only consider synonymy scores for pairs of different words.
+    Same word pairs have a synonymy score of 1."""
     trimmed_scores = scores_array[scores_array < 1]
     return trimmed_scores
 
 
+def normalize_array(scores_array):
+    scores_norm = (scores_array - np.min(scores_array))/np.ptp(scores_array)
+    scores_norm = scores_norm.round(decimals=6)  # clean up floating point errors and reduce significant digits
+    return scores_norm
+
+
 def calculate_statistics(scores_array):
+    """Calculate descriptive statistics for the scores distribution."""
     mu = np.mean(scores_array)
     sigma = np.std(scores_array)
     a_min = np.min(scores_array)
@@ -59,7 +70,7 @@ def calculate_statistics(scores_array):
 def make_output_filenames():
     """Write statistics and histogram figure to the specified directory."""
     hist_file = os.path.join(args.histogram_dir, 'distribution_histogram.png')
-    stats_file = os.path.join(args.clustering_dir, 'distribution_stats.txt')
+    stats_file = os.path.join(args.histogram_dir, 'distribution_stats.txt')
     return hist_file, stats_file
 
 
@@ -82,30 +93,37 @@ if __name__ == '__main__':
     assert len(scores_all) == len(scores_sorted)
     scores_array = make_array(scores_sorted)
     assert (scores_array.shape)[0] == len(scores_sorted)
-    scores_trimmed = trim_scores(scores_array)
+    scores_norm = normalize_array(scores_array)
+    assert scores_array.shape[0] == scores_norm.shape[0]
+    scores_trimmed = trim_scores(scores_norm)
     print(f'After removing scores = 1, there are {scores_trimmed.shape[0]} scores.')
     
     # Calculate statistics of the distribution.
     mu, sigma, a_min, a_max = calculate_statistics(scores_trimmed)
-    print(f'Mean of {scores_trimmed.shape[0]} scores is {mu}, with standard deviation {sigma}.')
-    print(f'The minimum is {a_min} and the maximum non-1 value is {a_max}.')
+    stats_printout = format_distribution_stats(mu, sigma, a_min, a_max)
 
     # Build the histogram figure.
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(14, 8.5))  #(width, height) in inches
     # Plot the histogram of the data.
     n, bins, patches = ax.hist(scores_trimmed, args.bin_count, density=1)
+    plt.figtext(0.02, 0.12, stats_printout, horizontalalignment='left', verticalalignment='center', fontsize=14)
+    plt.subplots_adjust(bottom=0.32, top=0.95, right=0.98, left=0.06)
     # Add a 'best fit' line.
     y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
         np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
     ax.plot(bins, y, '--')
     # Format the figure.
-    plt.rc('ytick',labelsize=16)
-    ax.set_xlabel('Scores', fontsize=16)
-    ax.set_ylabel('Probability density', fontsize=16)
+    y_label = 'Probability Density'
+    x_label = 'Synonymy Scores: ' + str(args.bin_count) + ' Bins'
+    ax.set_xlabel(x_label, fontsize=16)
+    ax.set_ylabel(y_label, fontsize=16)
     ax.set_title('Histogram of Synonymy Scores', fontsize=18)
 
     # Save the figure and statistics.
     hist_file, stats_file = make_output_filenames()
+    with open(stats_file, 'w') as f_stat:
+        f_stat.write(stats_printout)
+    plt.savefig(hist_file, format='png')
 
     # Display the figure.
     plt.show()
