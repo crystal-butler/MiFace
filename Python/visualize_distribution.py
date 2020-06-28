9ad8bf8f2748a2691e8001d7622bfb9f4f279a81
Crystal Butler <crystal.butler@arup.com>
2020-06-28 16:14:07 -0400

    calculate statistics of distribution

--- Python/visualize_distribution.py
+++ Python/visualize_distribution.py
@@ -33,24 +33,51 @@ def sort_scores(scores):
 
 def make_array(scores):
     scores_array = np.array(scores).astype(np.float)
+    scores_array = scores_array.round(decimals=6)
     return(scores_array)
+
+
+def trim_scores(scores_array):
+    trimmed_scores = scores_array[scores_array < 1]
+    return trimmed_scores
+
+
+def calculate_statistics(scores_array):
+    mu = np.mean(scores_array)
+    sigma = np.std(scores_array)
+    a_min = np.min(scores_array)
+    a_max = scores_array.max()
+    return mu, sigma, a_min, a_max
 
 
 if __name__ == '__main__':
     scores_all = concatenate_scores()
-    print(len(scores_all))
+    print(f'Read in {len(scores_all)} scores.')
     scores_sorted = sort_scores(scores_all)
     assert len(scores_all) == len(scores_sorted)
     scores_array = make_array(scores_sorted)
     assert (scores_array.shape)[0] == len(scores_sorted)
-
-    mu = np.mean(scores_array)
-    sigma = np.std(scores_array)
-    print(f'Mean of {scores_array.shape[0]} scores is {mu}, with standard deviation {sigma}.')
+    scores_trimmed = trim_scores(scores_array)
+    print(f'After removing scores = 1, there are {scores_trimmed.shape[0]} scores.')
+    
+    # Calculate statistics of the distribution.
+    mu, sigma, a_min, a_max = calculate_statistics(scores_trimmed)
+    print(f'Mean of {scores_trimmed.shape[0]} scores is {mu}, with standard deviation {sigma}.')
+    print(f'The minimum is {a_min} and the maximum non-1 value is {a_max}.')
 
     # Build the histogram figure.
     fig, ax = plt.subplots()
     # Plot the histogram of the data.
-    n, bins, patches = ax.hist(scores_array, args.bin_count, density=1)
+    n, bins, patches = ax.hist(scores_trimmed, args.bin_count, density=1)
+    plt.rc('ytick',labelsize=16)
+    # Add a 'best fit' line.
+    y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
+        np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
+    ax.plot(bins, y, '--')
+    # Format the figure.
+    ax.set_xlabel('Scores', fontsize=16)
+    ax.set_ylabel('Probability density', fontsize=16)
+    ax.set_title('Histogram of Synonymy Scores', fontsize=18)
 
-    # plt.show()
+    plt.show()
+    plt.close()
